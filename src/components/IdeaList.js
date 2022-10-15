3f31248ab367b9d8313f89d06de78eff3fea516c
kammanz <kaumilmanzoor@gmail.com>
2022-10-15 15:53:25 -0400

    more hotfixes

--- src/components/IdeaList.js
+++ src/components/IdeaList.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useState } from 'react';
-import styles from './Item.module.css';
+import styles from './IdeaList.module.css';
 
 const IdeaList = () => {
   const [ideas, setIdeas] = useState([]);
