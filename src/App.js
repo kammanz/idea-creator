b731f1e34c32372baa3abc87bbf6623236a0e4bd
kammanz <kaumilmanzoor@gmail.com>
2022-10-18 09:54:20 -0400

    updated styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import IdeaList from './components/IdeaList';
+import Header from './components/Header';
 
 function App() {
-  return <IdeaList />;
+  return (
+    <div className="container">
+      <Header />
+      <IdeaList />
+    </div>
+  );
 }
 
 export default App;
