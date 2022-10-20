74fba2324a57ef9f934c3039f1d71d2113f704b6
kammanz <kaumilmanzoor@gmail.com>
2022-10-20 12:05:14 -0400

    display creation date

--- src/components/ReactForm.js
+++ src/components/ReactForm.js
@@ -4,9 +4,24 @@ import styles from './Card.module.css';
 
 const ReactForm = () => {
   const [ideas, setIdeas] = useState([
-    { id: 1, title: 'title1', description: 'desc1' },
-    { id: 2, title: 'title2', description: 'desc2' },
-    { id: 3, title: 'title3', description: 'desc3' },
+    {
+      id: 1,
+      title: 'title1',
+      description: 'desc1',
+      date: { numeral: 123, currentDate: 'Monday Jan 7 2022' },
+    },
+    {
+      id: 2,
+      title: 'title2',
+      description: 'desc2',
+      date: { numeral: 123, currentDate: 'Tues Jan 8 2022' },
+    },
+    {
+      id: 3,
+      title: 'title3',
+      description: 'desc3',
+      date: { numeral: 125, currentDate: 'Wed Jan 9 2022' },
+    },
   ]);
 
   const { register, handleSubmit, watch, setFocus, reset } = useForm();
@@ -15,11 +30,23 @@ const ReactForm = () => {
     setFocus('title');
   }, [ideas]);
 
+  let dateObject = () => {
+    let date = new Date();
+    let numeral = date.getTime();
+    let currentDate = date.toLocaleString();
+
+    return {
+      numeral,
+      currentDate,
+    };
+  };
+
   const submitList = (data) => {
     let newIdea = {
       id: Math.floor(Math.random() * 1000),
       title: data.title,
       description: data.description,
+      date: dateObject(),
     };
 
     reset();
@@ -70,6 +97,7 @@ const ReactForm = () => {
                 defaultValue={description}
                 {...register(description)}></textarea>
               <button onClick={(e) => handleClick(e, id)}>Delete</button>
+              <p>Created/Updated: {idea.date.currentDate}</p>
             </fieldset>
           );
         })}
