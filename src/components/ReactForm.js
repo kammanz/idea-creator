065cf319079b272065dc2bd947162a3213b3531f
kammanz <kaumilmanzoor@gmail.com>
2022-10-26 11:33:39 -0400

    added formik

--- src/components/ReactForm.js
+++ src/components/ReactForm.js
@@ -1,135 +1,87 @@
-import React, { useState, useEffect } from 'react';
-import { useForm } from 'react-hook-form';
-import styles from './Card.module.css';
+import React from 'react';
+import { Formik, Form, Field, onChange, FieldArray } from 'formik';
 
-const ReactForm = () => {
-  const [ideas, setIdeas] = useState([]);
-  const [isSafeToReset, setIsSafeToReset] = useState(false);
-  const [isSaveButtonActive, setIsSaveButtonActive] = useState(false);
+// const Input = () => {
+//     return (
+//         <>
+//         <input
+//         </>
+//     )
+// }
 
-  const {
-    register,
-    handleSubmit,
-    watch,
-    setFocus,
-    // reset,
-    resetField,
-    // control,
-    // formState: { dirtyFields },
-    // getValues,
-  } = useForm();
+export const Form3 = () => (
+  <div>
+    <Formik
+      initialValues={{
+        // ideas: [{ id: 5, title: 'swsw', description: 'swsws' }],
+        // people: [{ id: '5', firstName: 'bob', lastName: 'bob2' }],
+        people: [{ id: 5, firstName: '', lastName: '' }],
+      }}
+      onSubmit={() => {}}>
+      {({ values, handleChange }) => (
+        <Form>
+          <FieldArray name="people">
+            {({ push }) => (
+              <div>
+                {values.people && values.people.length > 0 ? (
+                  values.people.map((person, index) => {
+                    console.log('ran');
+                    return (
+                      <div key={person.id}>
+                        <input
+                          type="text"
+                          name={`people[${index}].firstName`}
+                          value={person.firstName}
+                          onChange={handleChange}
+                        />
+                        <input
+                          type="text"
+                          name={`people[${index}].lastName`}
+                          value={person.lastName}
+                          onChange={handleChange}
+                        />
+                      </div>
+                    );
+                  })
+                ) : (
+                  <div>
+                    {' '}
+                    <input
+                      type="text"
+                      name={`values.people[0].firstName`}
+                      value={values.people[0].firstName}
+                      onChange={handleChange}
+                    />
+                    <input
+                      type="text"
+                      name={`values.people[0].lastName`}
+                      value={values.people[0].lastName}
+                      onChange={handleChange}
+                    />
+                  </div>
+                )}
+                <button
+                  type="button"
+                  onClick={() =>
+                    push({
+                      id: Math.random(Math.floor * 1000),
+                      firstName: '',
+                      lastName: '',
+                    })
+                  }>
+                  Add to list
+                </button>
+              </div>
+            )}
+          </FieldArray>
+          <div>
+            <button type="submit">Submit</button>
+          </div>
+          <pre>{JSON.stringify(values, null, 2)}</pre>
+        </Form>
+      )}
+    </Formik>
+  </div>
+);
 
-  // console.log('defaultValues', defaultValues);
-  // console.log('dirtyFields', dirtyFields);
-
-  // watch('title');
-
-  // useEffect(() => {
-  //   if (!isSafeToReset) return;
-  // resetField('title');
-  // resetField('description');
-  // }, [ideas]);
-
-  let dateObject = () => {
-    let date = new Date();
-    let numeral = date.getTime();
-    let currentDate = date.toLocaleString();
-
-    return {
-      numeral,
-      currentDate,
-    };
-  };
-
-  const onSubmit = (data) => {
-    console.log('data: ', data);
-    let newIdea = {
-      id: Math.floor(Math.random() * 1000),
-      title: data.title,
-      description: data.description,
-      date: dateObject(),
-    };
-
-    setIdeas([...ideas, newIdea]);
-    setIsSafeToReset(true);
-    resetField('title');
-    resetField('description');
-  };
-
-  const handleSaveClick = (e, id) => {
-    setIdeas((ideas) =>
-      ideas.map((idea) =>
-        id === idea.id ? (idea = { ...idea, date: dateObject() }) : idea
-      )
-    );
-  };
-
-  const onChange = (e) => {
-    console.log('', e.target.value);
-    // setIsSaveButtonActive(true);
-  };
-
-  const List = () => {
-    console.log('list ran');
-
-    return (
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <fieldset className={styles.card}>
-          <input
-            required
-            name="title"
-            placeholder="Title"
-            defaultValue={''}
-            {...register('title')}
-          />
-          <textarea
-            required
-            placeholder="Description"
-            maxLength={140}
-            defaultValue={''}
-            {...register('description')}></textarea>
-          <button type="submit">Create</button>
-        </fieldset>
-        {ideas.map((idea) => {
-          let { id, title, description } = idea;
-          return (
-            <fieldset className={styles.card} key={id}>
-              <input
-                required
-                name="title"
-                placeholder="Title"
-                defaultValue={title}
-                {...register(`${id}`)}
-              />
-              <textarea
-                required
-                name="description"
-                placeholder="Description"
-                maxLength={140}
-                defaultValue={description}
-                {...register(description)}
-                onChange={onChange}></textarea>
-              <button
-                onClick={(e) => handleSaveClick(e, id)}
-                disabled={isSaveButtonActive ? false : true}>
-                Save Changes
-              </button>
-              <button>Cancel Changes</button>
-              <button>Delete</button>
-              <p>Created/Updated: {idea.date.currentDate}</p>
-            </fieldset>
-          );
-        })}
-      </form>
-    );
-  };
-
-  return (
-    <div>
-      <div>React Form</div>
-      <List />
-    </div>
-  );
-};
-
-export default ReactForm;
+export default Form3;
