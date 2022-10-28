2932064dc19cfb27097b1601e2043afa1aee82c9
kammanz <kaumilmanzoor@gmail.com>
2022-10-28 12:43:35 -0400

    another update

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,39 +1,50 @@
 import React from 'react';
 import styles from './Card.module.css';
 
-const Card = ({
-  selectedIdea,
-  selectedIdea: { isTitleActive, isDescriptionActive },
-  title,
-  description,
-  date,
-  id,
-  handleDelete,
-  handleUpdate,
-  handleActivateTitle,
-  isSelectedIdea,
-  handleTitleChange,
-  handleDescriptionChange,
-  handleActivateDescription,
-  handleBlur,
-}) => {
+const Card = (
+  {
+    selectedIdea,
+    selectedIdea: { isTitleActive, isDescriptionActive },
+    title,
+    description,
+    date,
+    id,
+    handleDelete,
+    handleUpdate,
+    handleActivateTitle,
+    isSelectedIdea,
+    handleTitleChange,
+    handleDescriptionChange,
+    handleActivateDescription,
+    handleBlur,
+    handleCancel,
+  },
+  ref
+  // textareaRef
+) => {
+  const inputRef2 = React.useRef();
+  console.log('inputRef2', inputRef2);
   return (
     <div className={styles.card}>
       {isSelectedIdea && isTitleActive ? (
         <input
+          ref={ref}
           type="text"
           defaultValue={selectedIdea.title}
           onChange={handleTitleChange}
           onBlur={handleBlur}
         />
       ) : (
-        <div onClick={() => handleActivateTitle(id)}>title: {title}</div>
+        <div onClick={(e) => handleActivateTitle(e, id)}>title: {title}</div>
       )}
       {isSelectedIdea && isDescriptionActive ? (
         <input
+          // ref={textareaRef}
+          ref={ref}
           type="textarea"
           defaultValue={selectedIdea.description}
           onChange={handleDescriptionChange}
+          onBlur={handleBlur}
         />
       ) : (
         <div onClick={() => handleActivateDescription(id)}>
@@ -51,9 +62,21 @@ const Card = ({
         }>
         Save
       </button>
+      <button
+        onClick={handleCancel}
+        disabled={
+          (isSelectedIdea && isTitleActive) ||
+          (isSelectedIdea && isDescriptionActive)
+            ? false
+            : true
+        }>
+        Cancel
+      </button>
       <button onClick={() => handleDelete(id)}>Delete</button>
     </div>
   );
 };
 
-export default Card;
+const ForwardedCard = React.forwardRef(Card);
+
+export default ForwardedCard;
--- src/components/List.js
+++ src/components/List.js
@@ -12,6 +12,15 @@ const List = () => {
     setSelectedIdea({});
   }, [ideas]);
 
+  useEffect(() => {
+    if (inputRef.current !== undefined) {
+      console.log('inputRef, inside conditional: ');
+      inputRef && inputRef.current?.focus();
+    }
+  }, [selectedIdea]);
+
+  let inputRef = React.useRef();
+
   const handleSubmit = (values) => {
     const { title, description } = values;
     let date = new Date();
@@ -36,10 +45,12 @@ const List = () => {
   const handleUpdate = (id) => {
     console.log('handle update ran');
     console.log('selectedIdea.title', selectedIdea.title);
+    console.log('selectedIdea.description', selectedIdea.description);
     let date = new Date();
     let dateNum = date.getTime();
     let dateString = date.toLocaleString();
     let updatedList = [...ideas].map((idea) => {
+      console.log('inside map');
       return idea.id === id
         ? {
             ...idea,
@@ -54,13 +65,16 @@ const List = () => {
     setIdeas(updatedList);
   };
 
-  const handleActivateTitle = (id) => {
+  const handleActivateTitle = (e, id) => {
+    console.log('e.target', e.currentTarget);
     const selectedItem = ideas.find((idea) => idea.id === id);
     setSelectedIdea({
       ...selectedItem,
       isTitleActive: true,
       isDescriptionActive: false,
     });
+    // console.log(inputRef.current);
+    // inputRef.current?.focus();
   };
 
   const handleActivateDescription = (id) => {
@@ -82,6 +96,10 @@ const List = () => {
 
   const handleBlur = () => {
     console.log('handle blur');
+    setSelectedIdea({});
+  };
+
+  const handleCancel = () => {
     setSelectedIdea({});
   };
 
@@ -109,6 +127,8 @@ const List = () => {
                 handleTitleChange={handleTitleChange}
                 handleDescriptionChange={handleDescriptionChange}
                 handleBlur={handleUpdate}
+                handleCancel={handleCancel}
+                ref={inputRef}
               />
             </div>
           );
