7ad484b6510cdaf47d769c278e8290f507cdd7d2
kammanz <kaumilmanzoor@gmail.com>
2022-10-31 18:44:38 -0400

    added sort by newest and oldest

--- src/components/List.js
+++ src/components/List.js
@@ -4,6 +4,8 @@ import { useState, useEffect } from 'react';
 import Card from './Card';
 import Form from './Form';
 
+import { newest, oldest, compare } from '../services';
+
 const List = () => {
   const [ideas, setIdeas] = useState([]);
 
@@ -12,7 +14,7 @@ const List = () => {
   useEffect(() => {
     inputRef.current.focus();
     return;
-  }, [ideas]);
+  }, []);
 
   const handleSubmit = (values) => {
     const { title, description } = values;
@@ -54,10 +56,57 @@ const List = () => {
     setIdeas(updatedList);
   };
 
+  const handleSelectChange = (e) => {
+    console.log('select change ran');
+    console.log('e.target.value: ', e.target.value);
+
+    if (e.target.value === 'newest') {
+      let sortedArrayNew = [...ideas].sort(newest);
+      console.log('sortedArrayNew: ', sortedArrayNew);
+      setIdeas(sortedArrayNew);
+    } else if (e.target.value === 'oldest') {
+      let sortedArrayOld = [...ideas].sort(oldest);
+      console.log('sortedArrayOld: ', sortedArrayOld);
+      setIdeas(sortedArrayOld);
+    }
+
+    // let sortedArrayNew = [...ideas].sort(newest);
+    // console.log('sortedArrayNew: ', sortedArrayNew);
+    // setIdeas(sortedArrayNew);
+    // switch (e.target.value) {
+    //   case 'newest':
+    //     // code block
+    //     console.log('newest, ideas: ', ideas);
+    //     let sortedArrayNew = [...ideas].sort(newest);
+    //     console.log('sortedArrayNew: ', sortedArrayNew);
+    //     setIdeas(sortedArrayNew);
+    //     break;
+    //   case 'oldest':
+    //     // code block
+    //     // console.log('oldest, ideas: ', ideas);
+    //     let sortedArrayNew = [...ideas].sort(newest);
+    //     // let sortedArrayOld = [...ideas].sort(oldest);
+    //     console.log('sortedArrayOld: ', sortedArrayOld);
+    //     setIdeas(sortedArrayOld);
+    //     break;
+    //   default:
+    //   // code block
+    // }
+  };
+
   return (
     <>
       <h4>Create an Idea</h4>
       <Form handleSubmit={handleSubmit} inputRef={inputRef} />
+      <h4>Sort list</h4>
+      <form>
+        <select onChange={handleSelectChange}>
+          <option value="newest">newest</option>
+          <option value="oldest">oldest</option>
+          {/* <option value="by-ascending-alphabet">by-ascending-alphabet</option>
+          <option value="by-descending-alphabet">by-descending-alphabet</option> */}
+        </select>
+      </form>
       <h4>List of Ideas</h4>
       {ideas &&
         ideas.map((idea, i) => {
--- src/services/index.js
+++ src/services/index.js
@@ -1,9 +1,29 @@
-const customSort = (a, b) => {
-  const dateA = a.date.currentDate;
-  const dateB = b.date.currentDate;
+export const newest = (a, b) => {
+  let dateA = a.dateNum;
+  console.log('a.dateNum: ', a.dateNum);
+  let dateB = b.dateNum;
+  console.log('b.dateNum: ', b.dateNum);
   if (dateA < dateB) {
     return 1;
   } else if (dateA > dateB) {
+    return -1;
+  } else {
+    return 0;
+  }
+};
+
+export const compare = (a, b) => {
+  return a.dateNum - b.dateNum;
+};
+
+export const oldest = (a, b) => {
+  let dateA = a.dateNum;
+  console.log('a.dateNum: ', a.dateNum);
+  let dateB = b.dateNum;
+  console.log('b.dateNum: ', b.dateNum);
+  if (dateA > dateB) {
+    return 1;
+  } else if (dateA < dateB) {
     return -1;
   } else {
     return 0;
